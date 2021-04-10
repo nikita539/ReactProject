15e994f02a095e515fc85207c46f542a2773dad4
artem <70851610+artemdov@users.noreply.github.com>
2021-04-10 14:57:02 +0300

    new commit

--- my-app/src/components/register/register.tsx
+++ my-app/src/components/register/register.tsx
@@ -1,10 +1,13 @@
 import React, {useState} from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {Redirect} from "react-router-dom";
 import {RegisterTC} from "../../store/register-reducer";
 import RegisterForm from "./registerForm";
+import {AppRootStateType} from "../../store/store";
 
 const Register = () => {
     const dispatch = useDispatch()
+    const isRegisteredUser = useSelector<AppRootStateType, boolean>(state => state.register.isRegistered)
 
     let [email, setEmail] = useState('')
     let [password, setPassword] = useState('')
@@ -12,9 +15,16 @@ const Register = () => {
 
 
     const signUp = () => {
+
         if (password === confirmPassword) {
             dispatch(RegisterTC(email, password))
+        } else {
+            return
         }
+        if(!isRegisteredUser) {
+            return < Redirect to={"/Log_in"}/>
+        }
+
     }
 
     const setEmailChange = (value: string) => {
@@ -43,10 +53,6 @@ const Register = () => {
     </div>
 
 
-
-
 }
 
 export default Register;
-//popii
-////fjfjgjgjgkjg
