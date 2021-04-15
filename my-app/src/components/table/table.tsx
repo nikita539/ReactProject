b43502c9c04ee635e996e855f2f11655bef889ce
nikita539 <evteev_n@inbox.ru>
2021-04-15 20:28:02 +0300

    add

--- my-app/src/components/table/table.tsx
+++ my-app/src/components/table/table.tsx
@@ -19,7 +19,11 @@ const Table = () => {
             <td>{t.user_name}</td>
             <td>{t.name}</td>
             <td>{t.updated}</td>
-            <td><button>delete</button></td>
+            <td style={{display:"flex",justifyContent:"space-around"}}>
+                <button type="button" className="btn btn-outline-primary btn-sm">Add</button>
+                <button type="button" className="btn btn-outline-primary btn-sm">Delete</button>
+                <button type="button" className="btn btn-outline-primary btn-sm">Change</button>
+            </td>
         </tr>
     })
 
