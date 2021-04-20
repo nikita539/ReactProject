90b6dd4bd41170f1da2e7a41f85a79fa5df7c0dd
artem <70851610+artemdov@users.noreply.github.com>
2021-04-20 15:35:57 +0300

    add to pagination selector

--- my-app/src/components/pagination/pagination.tsx
+++ my-app/src/components/pagination/pagination.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {ChangeEvent} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../store/store";
 import style from './pagination.module.css'
@@ -14,29 +14,76 @@ const Pagination = () => {
 
     const dispatch = useDispatch()
 
+
     const CountOfPages = Math.ceil(cardPacksTotalCount / pageCount)
-    const pages = []
+    let pages = []
     for (let i = 1; i <= CountOfPages; i++) {
         pages.push(i)
     }
-    const handlerPagination = (pageNumber: number) => {
+    //1...4 (5) 6...10
+    if((page + 4) < CountOfPages ){
+        pages[page + 2] = (
+            <span key={page + 3} className={style.ButtonStyle}>
+                    - ... -
+                </span>
+        )
+        pages = pages.filter((o,i) => i < (page + 3) || i === (CountOfPages - 1))
+    }
+    if(page > 5) {
+        pages[1] = (
+            <span key={2} className={style.ButtonStyle}>
+                  - ... -
+        </span>
+        )
+        pages = pages.filter((o, i) => i < 2 || i > page - 4)
+    }
+    const handlerPagination: (pageNumber: number| any) => void = (pageNumber: number) => {
         dispatch(pageAC(pageNumber))
         dispatch(paginationTC(pageNumber, pageCount))
     }
+    const handlerChangeSelect =(e: ChangeEvent<HTMLSelectElement>) => {
+        dispatch(paginationTC(page, Number(e.currentTarget.value)))
+    }
+
+
+
+
+
 
 
     return (
 
-        <div>
+        <div className={style.SelectWithButtons}>
             <div>
-                {
-                    pages.map((p, id) =>
-                        <button key={id} onClick={(e) => handlerPagination(p)} className={page === p ? style.SelectPage : ''}>{p}</button>
-                    )
-                }
+
+                <select className={style.Selector} value={pageCount} onChange={(e) => handlerChangeSelect} >
+                    <option value={4}>4</option>
+                    <option value={10}>10</option>
+                    <option value={20}>20</option>
+                    <option value={50}>50</option>
+                </select>
             </div>
+
+
+                {
+                    pages.map((p, i) =>
+                        <button key={i} onClick={(e) =>
+                            handlerPagination(p)} className={page === p ? style.SelectPage : ''}>
+                            {p}
+                        </button>
+                    )
+
+
+                }
+
+
+
+
+
+
         </div>
     )
+
 
 }
 
